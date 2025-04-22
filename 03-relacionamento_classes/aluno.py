9b68e4d228f69f1e89f479d63ffdc7c6f12c4d27
emanoel <eoemanoel@gmail.com>
2025-04-22 15:22:57 -0300

    Implement Disciplina and Aluno classes with properties and string representation

---
+++ 03-relacionamento_classes/aluno.py
@@ -0,0 +1,58 @@
+
+class Disciplina:
+    def __init__(self,nome,carga_horaria):
+        self.__nome = nome
+        self.__carga_horaria = carga_horaria
+    
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, nome_novo):
+        self.__nome = nome_novo
+
+    @property 
+    def carga_horaria(self):
+        return self.__carga_horaria
+    
+    @carga_horaria.setter
+    def carga_horaria(self, nova_carga_horaria):
+        self.__carga_horaria = nova_carga_horaria
+
+    def __str__(self):
+        return f'{self.__nome} ({self.__carga_horaria})'
+        
+
+class Aluno:
+    def __init__(self,nome, disciplina):
+        self.__nome = nome
+        self.__disciplina = disciplina
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
+
+    @property
+    def disciplina(self):
+        return self.__disciplina
+
+    @disciplina.setter
+    def disciplina(self, nova_disciplina):
+        self.__disciplina = nova_disciplina
+
+    def __str__(self):
+        return f'\nAluno - {self.__nome}\nDisciplina - ({self.__disciplina})\n'
+
+# relacionamento 
+
+aluno1 = Aluno('João', Disciplina('Estrutura de Dados', 67))
+aluno2 = Aluno('Maria', Disciplina('Banco de dados I',87))
+
+print('Aluno 1:', aluno1)
+
+print('Aluno 2:', aluno2)
---
+++ linked-list/01_LL.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.length += 1
+    
+    def __str__(self):
+       
+        # defining a blank res variable
+        res = ""
+         
+        # initializing ptr to head
+        ptr = self.head
+         
+       # traversing and adding it to res
+        while ptr: #while is != none
+            res += str(ptr.value) + ", "
+            ptr = ptr.next
+ 
+       # removing trailing commas
+        res = res.strip(", ")
+         
+        # chen checking if 
+        # anything is present in res or not
+        if len(res):
+            return "[" + res + "]"
+        else:
+            return "[]"
+
+new_node = LinkedList()
+
+
+
+for n in range(1,51):
+    new_node.append(n)
+
+new_node.append(1000)
+
+print(new_node.head)
+print(new_node.head.value)
+
+print(new_node.tail.value)
+print(new_node.length)
+
+print(new_node)
